da08389856527caa9636e64e957e3b3478628b68
Guilherme Berger <guilherme.berger@gmail.com>
2014-04-03 00:59:38 -0300

    validate summoner

--- app/controllers/summoners_controller.rb
+++ app/controllers/summoners_controller.rb
@@ -4,10 +4,19 @@ class SummonersController < ApplicationController
     return unless current_user
     return if current_user.summoner.present?
 
-    @summoner = Summoner.new(name: params[:summoner_name], user: current_user)
+    @summoner = Summoner.new(name: summoner_params[:name], user: current_user)
 
-    @summoner.save
+    if @summoner.save
+      redirect_to user_path_with_nickname_maybe(current_user)
+    else
+      redirect_to user_path_with_nickname_maybe(current_user), alert: 'Summoner Name inválido!'
+    end
 
-    redirect_to user_path_with_nickname_maybe(current_user)
+  end
+
+private
+
+  def summoner_params
+    params.require('summoner').permit('name')
   end
 end
