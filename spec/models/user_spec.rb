1c2dc7fd0c4a1e8b51197663cda328c3e881a5b1
Guilherme Berger <guilherme.berger@gmail.com>
2014-04-07 11:57:34 -0300

    test User#identifier

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,16 @@ describe User do
   describe 'relations' do
     it { should have_one(:summoner) }
   end
+
+  describe '#identifier' do
+    it 'is the nickname when available' do
+      user = build(:user)
+      user.identifier.should == user.nickname
+    end
+
+    it 'is the id otherwise' do
+      user = build(:user, nickname: nil)
+      user.identifier.should == user.id
+    end
+  end
 end
